057bad68fbaf7b5139644232f92acfa992a2a9f6
KtoK21 <rlgus01003@gmail.com>
2019-04-17 16:00:24 +0900

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int Resource = 10;
+    public int Health = 100;
+
+    public int LeftLineCount = 0;
+    public int MiddleLineCount = 0;
+    public int RightLineCount = 0;
+
+    float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 1.0f)
+        {
+            Resource++;
+            timer = 0.0f;
+        }
+    }
+
+    public int LineAdd(string line)
+    {
+        if (Resource <= 0)
+            return -1;
+        else
+        {
+            Resource--;
+
+            switch (line)
+            {
+                case "Left":
+                    return ++LeftLineCount;
+                case "Middle":
+                    return ++MiddleLineCount;
+                case "Right":
+                    return ++RightLineCount;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+
+    public Text ResourceText;
+    public Text HealthText;
+    public Text LeftLineText;
+    public Text MiddleLineText;
+    public Text RightLineText;
+    public Text TimeText;
+    public float remainTime = 300.0f;
+
+    public Button LeftLine;
+    public Button MiddleLine;
+    public Button RightLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeDisplay();
+        ResourceDisplay();
+        HealthDisplay();
+    }
+
+    public void OnLinePress(string line)
+    {
+        int Count = GameObject.Find("GameManager").GetComponent<GameManager>().LineAdd(line);
+        
+        //Exception
+        if(Count == -1)
+        {
+            return;
+        }
+
+        switch (line)
+        {
+            case "Left":
+                LeftLineText.text = Count.ToString();
+                break;
+            case "Middle":
+                MiddleLineText.text = Count.ToString();
+                break;
+            case "Right":
+                RightLineText.text = Count.ToString();
+                break;
+        }            
+    }
+
+    void TimeDisplay()
+    {
+        remainTime -= Time.deltaTime;
+        int sec = Mathf.FloorToInt(remainTime % 60);
+
+        if (sec < 10)
+            TimeText.text = "Time : " + Mathf.FloorToInt(remainTime / 60) + ":0" + sec;
+        else
+            TimeText.text = "Time : " + Mathf.FloorToInt(remainTime / 60) + ":" + sec;
+    }
+
+    void ResourceDisplay()
+    {
+        ResourceText.text = "Resource : " + GameObject.Find("GameManager").GetComponent<GameManager>().Resource.ToString();
+    }
+
+    void HealthDisplay()
+    {
+        HealthText.text = "Health : " + GameObject.Find("GameManager").GetComponent<GameManager>().Health.ToString();
+    }
+
+
+
+}
